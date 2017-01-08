59b196c05bc5aaf46b3e86783328f9906f9da9bb
BaiJiFeiLong <baijifeilong@gmail.com>
2017-01-08 20:46:48 +0800

    Create this project

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+#include <tchar.h>
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	MessageBox(NULL, _T("HELLO"), NULL, 0);
+}
