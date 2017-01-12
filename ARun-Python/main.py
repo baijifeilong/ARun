108143ffb396c1c46b8422ed53a826f19620b641
BaiJiFeiLong <baijifeilong@gmail.com>
2017-01-13 00:03:44 +0800

    Create Add, Delete, Edit buttons, and create edit dialog

--- ARun-Python/main.py
+++ ARun-Python/main.py
@@ -8,6 +8,9 @@ class MyWindow:
     def __init__(self):
         self.hinst = GetModuleHandle(None)
         self.hwnd_list_box = None
+        self.hwnd_command = None
+        self.hwnd_add = None
+        self.hwnd_delete = None
         self.hwnd_edit = None
         self.file_name = 'commands.txt'
         self.commands = {}
@@ -20,17 +23,9 @@ class MyWindow:
         wc.lpfnWndProc = self.wnd_proc
 
         self.hwnd = CreateWindow(
-            RegisterClass(wc),
-            "Hello",
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            200,
-            300,
-            0,
-            0,
-            self.hinst,
-            None
+            RegisterClass(wc), "Hello", WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, 200, 300,
+            0, 0, self.hinst, None
         )
 
         SendMessage(self.hwnd, WM_CREATE, 0, 0)
@@ -51,16 +46,21 @@ class MyWindow:
                 if HIWORD(wparam) == LBN_DBLCLK:
                     print "Clicked"
                     self.exec_selected_command()
-            elif lparam == self.hwnd_edit:
+            elif lparam == self.hwnd_command:
                 if HIWORD(wparam) == EN_CHANGE:
                     print "Change"
                     self.update_list_box()
+            elif lparam == self.hwnd_add:
+                MyDialog(self.hwnd)
+            elif lparam == self.hwnd_edit:
+                MyDialog(self.hwnd)
         elif message == WM_CHAR:
             print 'char'
             if wparam == VK_RETURN:
                 self.exec_selected_command()
             elif wparam == VK_ESCAPE:
-                PostQuitMessage(0)
+                # PostQuitMessage(0)
+                MyDialog(self.hwnd)
         elif message == WM_DROPFILES:
             print DragQueryFile(wparam, 0)
         return DefWindowProc(hwnd, message, wparam, lparam)
@@ -80,8 +80,12 @@ class MyWindow:
     def update_layout(self):
         rect = GetClientRect(self.hwnd)
         edit_height = 20
-        MoveWindow(self.hwnd_edit, 0, 0, rect[2], edit_height, True)
+        button_size = 20
+        MoveWindow(self.hwnd_command, 0, 0, rect[2] - button_size * 3, edit_height, True)
         MoveWindow(self.hwnd_list_box, 0, edit_height, rect[2], rect[3] - edit_height, True)
+        MoveWindow(self.hwnd_add, rect[2] - button_size * 3, 0, edit_height, edit_height, True)
+        MoveWindow(self.hwnd_delete, rect[2] - button_size * 2, 0, edit_height, edit_height, True)
+        MoveWindow(self.hwnd_edit, rect[2] - button_size * 1, 0, edit_height, edit_height, True)
 
     def center_window(self):
         rect = GetWindowRect(self.hwnd)
@@ -91,7 +95,7 @@ class MyWindow:
         MoveWindow(self.hwnd, (desktop_rect[2] - width) / 2, (desktop_rect[3] - height) / 2, width, height, True)
 
     def init_layout(self):
-        self.hwnd_edit = CreateWindow(
+        self.hwnd_command = CreateWindow(
             'EDIT', None, WS_CHILD | WS_VISIBLE | WS_BORDER,
             0, 0, 0, 0,
             self.hwnd, 0, self.hinst, None
@@ -100,20 +104,35 @@ class MyWindow:
             'LISTBOX', None, WS_CHILD | WS_VISIBLE | WS_VSCROLL | LBS_NOTIFY,
             0, 0, 0, 0,
             self.hwnd, 0, self.hinst, None)
+        self.hwnd_add = CreateWindow(
+            'BUTTON', '+', WS_CHILD | WS_VISIBLE,
+            0, 0, 0, 0,
+            self.hwnd, 0, self.hinst, None
+        )
+        self.hwnd_delete = CreateWindow(
+            'BUTTON', '-', WS_CHILD | WS_VISIBLE,
+            0, 0, 0, 0,
+            self.hwnd, 0, self.hinst, None
+        )
+        self.hwnd_edit = CreateWindow(
+            'BUTTON', 'E', WS_CHILD | WS_VISIBLE,
+            0, 0, 0, 0,
+            self.hwnd, 0, self.hinst, None
+        )
 
         self.update_layout()
         self.center_window()
         self.update_list_box()
 
     def update_list_box(self):
-        SetFocus(self.hwnd_edit)
+        SetFocus(self.hwnd_command)
         SendMessage(self.hwnd_list_box, LB_RESETCONTENT)
 
         commands = self.commands.items()
         commands.sort()
         count = 0
         for path, name in sorted(self.commands.items(), key=lambda item: item[1]):
-            if self.match(GetWindowText(self.hwnd_edit), name):
+            if self.match(GetWindowText(self.hwnd_command), name):
                 SendMessage(self.hwnd_list_box, LB_ADDSTRING, 0, name)
                 SendMessage(self.hwnd_list_box, LB_SETITEMDATA, count, sorted(self.commands.keys()).index(path))
                 count += 1
@@ -135,6 +154,27 @@ class MyWindow:
         return True
 
 
+class MyDialog:
+    def __init__(self, hwnd):
+        dt = [('Add new command', (0, 0, 120, 60), WS_OVERLAPPEDWINDOW),
+              ['STATIC', 'Name:', -1, (5, 5, 45, 9), WS_CHILD | WS_VISIBLE],
+              ['EDIT', None, 1, (50, 5, 65, 9), WS_CHILD | WS_VISIBLE],
+              ['STATIC', 'Path:', -1, (5, 25, 50, 9), WS_CHILD | WS_VISIBLE],
+              ['EDIT', None, 1, (50, 25, 65, 9), WS_CHILD | WS_VISIBLE],
+              ['BUTTON', 'OK', 0, (45, 44, 30, 10), WS_CHILD | WS_VISIBLE]]
+        DialogBoxIndirect(None, dt, hwnd, self.DlgProc)
+
+    def DlgProc(self, hwnd, msg, wparam, lparam):
+        if (msg == WM_INITDIALOG):
+            print 'init'
+            DragAcceptFiles(hwnd, True)
+        elif (msg == WM_CLOSE):
+            EndDialog(hwnd, 0)
+        elif msg == WM_DROPFILES:
+            path = DragQueryFile(wparam, 0)
+            SetDlgItemText(hwnd, 1, path)
+
+
 wnd = MyWindow()
 while True:
     _, msg = GetMessage(None, 0, 0)
@@ -145,7 +185,7 @@ while True:
             if msg[2] in (VK_RETURN, VK_ESCAPE):
                 msg[0] = wnd.hwnd
             else:
-                msg[0] = wnd.hwnd_edit
+                msg[0] = wnd.hwnd_command
             msg = tuple(msg)
         TranslateMessage(msg)
         DispatchMessage(msg)
